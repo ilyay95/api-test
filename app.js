91a3b3aecabbee92c1cc5b3e540423d10d4b8e1f
ilyay95 <ilyay95@mail.ru>
2020-12-08 12:16:57 +0300

    feat:

--- app.js
+++ app.js
@@ -1,49 +1,55 @@
 const express = require("express");
+const Sequelize = require("sequelize");
 const bodyParser = require("body-parser");
-const User = require('./models/index');
+
 
 var app = express();
+const db = new Sequelize("postgres", "postgres", "1234567", {
+  dialect: "postgres",
+  host: "localhost",
+  define: {
+    timestamps: false
+  }
+});
 
-const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.listen(3000, () => {
    console.log(`Express server listening on port 3000...`);
 });
 
  
-app.get("/users", function(req, res){
-    User.findAll({raw:true}).then(users=>{
-        res.send(users);
-      }).catch(err=>console.log(err));
-});
+app.get( "/posts", (req, res) =>
+db.post.findAll().then( (result) => res.json(result) )
+);
 
+app.get( "/post/:id", (req, res) =>
+db.post.findByPk(req.params.id).then( (result) => res.json(result))
+);
 
-app.post("/create", urlencodedParser, function (req, res) {  
-    const username = req.body.name;
-    const userage = req.body.age;
+app.post("/post", (req, res) => 
+db.post.create({
+  title: req.body.title,
+  content: req.body.content
+}).then( (result) => res.json(result) )
+);
 
-    User.create({ name: username, age: userage}).then(()=>{
-      res.send();
-    }).catch(err=>console.log(err));
-});
+app.put( "/post/:id", (req, res) =>
+db.post.update({
+  title: req.body.title,
+  content: req.body.content
+},
+{
+  where: {
+    id: req.params.id
+  }
+}).then( (result) => res.json(result) )
+);
 
-app.post("/create/tom", urlencodedParser, function (req, res) {
-    User.create({
-        name: "Tom",
-        age: 35
-      }).then(res=>{
-        res.send();
-      }).catch(err=>console.log(err));
-});
-
-
-app.post("/delete/:id", function(req, res){  
-
-    const userid = req.params.id;
-
-    User.destroy({where: {id: userid} }).then(() => {
-      res.send();
-    }).catch(err=>console.log(err));
-  });
-
+app.delete( "/post/:id", (req, res) =>
+db.post.destroy({
+  where: {
+    id: req.params.id
+  }
+}).then( (result) => res.json(result) )
+);
   module.exports.app = app;
