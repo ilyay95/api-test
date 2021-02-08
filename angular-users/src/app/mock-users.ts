4abd630d9c723b9b576d9d3f582c1dfccb087feb
ilyay95 <ilyay95@mail.ru>
2021-02-08 20:46:43 +0300

    users: create angular app

---
+++ angular-users/src/app/mock-users.ts
@@ -0,0 +1,14 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+  { id: 11, name: 'Dr Nice', age:11 },
+  { id: 12, name: 'Narco', age:11 },
+  { id: 13, name: 'Bombasto', age:11 },
+  { id: 14, name: 'Celeritas', age:11 },
+  { id: 15, name: 'Magneta', age:11 },
+  { id: 16, name: 'RubberMan', age:11 },
+  { id: 17, name: 'Dynama', age:11 },
+  { id: 18, name: 'Dr IQ', age:11 },
+  { id: 19, name: 'Magma', age:11 },
+  { id: 20, name: 'Tornado', age:11 }
+];
