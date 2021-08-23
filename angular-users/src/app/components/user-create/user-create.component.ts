0ce415be2a337bd90aa286ec53cbb8adac0dea47
ilyay95 <ilyay95@mail.ru>
2021-08-23 20:07:29 +0300

    fix conflict

--- angular-users/src/app/components/user-create/user-create.component.ts
+++ angular-users/src/app/components/user-create/user-create.component.ts
@@ -11,7 +11,7 @@ export class UserCreateComponent implements OnInit {
   user = {
     firstName: '',
     age: '',
-    professionId:'',
+    professionId: '',
     logo: ''
   };
   submitted = false;
